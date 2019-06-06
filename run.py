d3ebc1517d373ba34d55b2b02880e5f5070e4d77
Daniel Ellis <ellis.daniel0@gmail.com>
2019-06-06 14:35:22 +0000

    added BLOCK content for PY and a base HTML page

--- run.py
+++ run.py
@@ -1,16 +1,32 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/") # @ is a pyNotation
 def index():
-    return "Hello, World"
+    return render_template("index.html")
     
-#Flask triggers the index function underneath.
+#Flask injects the @app.route functions, these are called within the application and then
+#as long as these are within the folder of "Templates" then the files will load with the HTML 
+# Being directed to the browser.
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route('/careers')
+def careers():
+    return render_template("careers.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
          debug=True)
+    ## Never have the above argument = true with deployed projects or anything LIVE
+    # as this opens up the possiblity for bad defensive development.
     
